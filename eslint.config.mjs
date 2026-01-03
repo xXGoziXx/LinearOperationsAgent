62dc034d3ec7cf815aa1a25337800d60fe8b8286
Godstime Osarobo <nosarobo@gmail.com>
2026-01-03 07:25:47 +0000

    fix: Update lint script in package.json to use eslint for improved code quality checks

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+export default defineConfig([
+    ...compat.extends("next/core-web-vitals"),
+]);
